344d217b0f2a81b20154f8bf711cf68d0fb37204
Vyacheslav Korotach <vkorotach@gmail.com>
2019-08-16 01:44:22 +0300

    refund is OK 01

--- eos_communicator.py
+++ eos_communicator.py
@@ -4,16 +4,22 @@ import json
 import time
 import paho.mqtt.client as mqtt
 from eospy.cleos import Cleos
+import eospy.cleos
+import eospy.keys
+import pytz
 
 message = 'ON'
+debug = True
 topic_sub1 = 'f'
 topic_pub1 = 'f2'
 eos_endpoint = 'https://eos.greymass.com:443'
+depth = 33
 mqtt_host = 'korotach.com'
 mqtt_user = 'igor'
 mqtt_password = 'igor1315'
 topic_pub2 = 'f3'
 vendor_account = 'wealthysnake'
+active_privat_key = os.environ['WINE_VENDOR_PRIVAT_KEY']
 
 
 def on_connect(mosq, obj, flags, rc):
@@ -79,31 +85,61 @@ def is_json(myjson):
 def get_last_actions():
     out = {}
     ce = Cleos(url=eos_endpoint)
-    actions = ce.get_actions(vendor_account, pos=-1, offset=-12)
+    actions = ce.get_actions(vendor_account, pos=-1, offset=-depth)
     if 'actions' in actions.keys():
         out = actions['actions']
     memos = []
     for s in out:
         receiver = s['action_trace']['receipt']['receiver']
-        recv_sequence = s['action_trace']['receipt']['recv_sequence']
         data = s['action_trace']['act']['data']
-        if receiver == vendor_account \
+        if s['action_trace']['act']['name'] == 'transfer' \
+                and receiver == vendor_account \
                 and 'to' in data.keys() \
                 and data['to'] == vendor_account \
-                and 'from' in data.keys()\
-                and 'quantity' in data.keys()\
+                and 'from' in data.keys() \
+                and 'quantity' in data.keys() \
                 and (data['quantity'].find('EOS') != -1 or data['quantity'].find('KNYGA') != -1):
-            data['recv_sequence'] = recv_sequence
+            data['recv_sequence'] = s['action_trace']['receipt']['recv_sequence']
+            data['account'] = s['action_trace']['act']['account']
             memos.append(data)
-            # if 'memo' in data.keys() and data['memo'] != "":
-            #     if is_json(data['memo']):
-            #         d = json.loads(data['memo'])
-            #         if d['latitude'] != '':
-            #             d['humidity'] = d['humidity_%']
-            #             memos.append(d)
-            #    memos = [d] + memos
     # memos.reverse()
     return memos
+
+
+def refund(action, amount, memo):
+    ce = Cleos(url=eos_endpoint)
+
+    arguments = {
+        "from": action['to'],  # sender
+        "to": action['from'],  # receiver
+        "quantity": str(amount) + ' ' + action['quantity'].split(' ')[1],  # In Token
+        "memo": memo,
+    }
+    payload = {
+        "account": action['account'],
+        "name": 'transfer',
+        "authorization": [{
+            "actor": vendor_account,
+            "permission": 'active',
+        }],
+    }
+    # Converting payload to binary
+    data = ce.abi_json_to_bin(payload['account'], payload['name'], arguments)
+    # Inserting payload binary form as "data" field in original payload
+    payload['data'] = data['binargs']
+    # final transaction formed
+    trx = {"actions": [payload]}
+    import datetime as dt
+    trx['expiration'] = str((dt.datetime.utcnow() + dt.timedelta(seconds=60)).replace(tzinfo=pytz.UTC))
+    # use a string or EOSKey for push_transaction
+    # key = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+    # use EOSKey:
+    key = eospy.keys.EOSKey(active_privat_key)
+    resp = ce.push_transaction(trx, key, broadcast=True)
+    # print('------------------------------------------------')
+    # print(resp)
+    # print('------------------------------------------------')
+    return resp
 
 
 mqttc = mqtt.Client()
@@ -126,27 +162,45 @@ if last_actions:
     last_actions.reverse()
     init_action_number = last_actions[0]['recv_sequence']
 print('last processed transaction number is ', init_action_number)
+last_processed_action = init_action_number
 vendor_EOS_balance = get_EOS_balance(vendor_account)
 vendor_EOS_balance_initial = vendor_EOS_balance
 vendor_KNYGA_balance = get_KNYGA_balance(vendor_account)
 vendor_KNYGA_balance_initial = vendor_KNYGA_balance
 state = 'Start'
+if debug: print('state = ', state)
 
 while state != 'Stop':
 
     # waiting for transaction
     while vendor_EOS_balance == vendor_EOS_balance_initial and vendor_KNYGA_balance == vendor_KNYGA_balance_initial:
         state = 'Waiting for transaction'
+        if debug: print('state = ', state)
         vendor_EOS_balance = get_EOS_balance(vendor_account)
-        print('vendor EOS balance = ', vendor_EOS_balance)
+        if debug: print('vendor EOS balance = ', vendor_EOS_balance)
         vendor_KNYGA_balance = get_KNYGA_balance(vendor_account)
-        print('vendor KNYGA balance = ', vendor_KNYGA_balance)
+        if debug: print('vendor KNYGA balance = ', vendor_KNYGA_balance)
         #    mqttc.publish(topic_pub2, '1235')
         time.sleep(3)
 
-    # detecting of transaction to process
+    # detecting transaction to be processed
     state = 'transactions parsing'
+    if debug: print('state = ', state)
     last_actions = get_last_actions()
-    print(last_actions)
+    if debug: print('last actions: ', last_actions)
+    actions_to_process = []
+    for n in last_actions:
+        if n['recv_sequence'] > last_processed_action:
+            actions_to_process.append(n)
+    if debug: print('action_to_process: ', actions_to_process)
+
+    # processing transactions
+    state = 'processing transactions'
+    if debug: print('state = ', state)
+    for n in actions_to_process:
+        amount = float(n['quantity'].split(' ')[0])
+        resp = refund(n, amount, 'refund code 01')
+        if debug: print('resp: ', resp)
 
     state = 'Stop'
+    if debug: print('state = ', state)
